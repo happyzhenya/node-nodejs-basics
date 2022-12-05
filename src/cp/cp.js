4ba9be66c584a95fe1f6445aaef0bba193ce364b
Sushkevich Zhenya <happy1@list.ru>
2022-12-05 18:12:40 +0300

    feat: solve cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,36 @@
+import { createReadStream } from "fs";
+import path from "path";
+import url from "url";
+import { spawn } from "child_process";
+import "./files/script.js";
+import { allowedNodeEnvironmentFlags } from "process";
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  const src = dirname + "/files/scripts.js";
+  const child = spawn("node", [src, ...args.split(" ")]);
+  process.stdin.on("data", (message) => {
+    child.stdin.write(message);
+
+    child.stdout.on("data", (data) => {
+      console.log(`stdout:\n ${data}`);
+    });
+    /*child.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });*/
+
+    child.on("error", (error) => {
+      console.error(`error: ${error.message}`);
+    });
+
+    child.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess("--all");
