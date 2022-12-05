48e7ff997e6dd489c1af5412ba9acf6293fda583
Sushkevich Zhenya <happy1@list.ru>
2022-12-05 16:20:29 +0300

    feat: solve cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const temp = [];
+  process.argv.forEach((i, index, arr) => {
+    if (i.startsWith("--")) {
+      temp.push(`${i.replace("--", " ")} is ${arr[index + 1]}`);
+    }
+  });
+  console.log(temp.join(","));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  //console.log(process.env);
+  const temp = [];
+  Object.entries(process.env).forEach(([key, value]) => {
+    if (key.startsWith("RSS_")) {
+      temp.push(`${key}=${value}`);
+    }
+  });
+  console.log(temp.join("; "));
 };
 
 parseEnv();
