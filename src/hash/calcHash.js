ea5f93e52cc895bc9aabac451e440ec627a8f2b9
Sushkevich Zhenya <happy1@list.ru>
2022-12-05 14:57:10 +0300

    feat: solve hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import crypto from "crypto";
+import { readFile } from "fs/promises";
+import path from "path";
+import url from "url";
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const src = dirname + "/files/fileToCalculateHashFor.txt";
+  try {
+    const data = await readFile(src, "utf-8");
+    console.log(data);
+    const hashSha256 = crypto.createHash("sha256");
+    hashSha256.update(data);
+    console.log(hashSha256.digest("hex"));
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await calculateHash();
