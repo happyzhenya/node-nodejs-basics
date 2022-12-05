9910e2f466119fb8be4d64bd215379eb073ee22b
Sushkevich Zhenya <happy1@list.ru>
2022-12-05 14:59:15 +0300

    feat: solve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import url from "url";
+import { createGzip } from "zlib";
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const srcIn = dirname + "/files/fileToCompress.txt";
+  const srcOut = dirname + "/files/archive.gz";
+  const inputFile = createReadStream(srcIn);
+  const outputFile = createWriteStream(srcOut);
+  inputFile.pipe(createGzip()).pipe(outputFile);
+  console.log("compresses file fileToCompress.txt ");
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import url from "url";
+import { createUnzip } from "zlib";
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const srcOut = dirname + "/files/fileToCompress.txt";
+  const srcIn = dirname + "/files/archive.gz";
+
+  const inputFile = createReadStream(srcIn);
+  const outputFile = createWriteStream(srcOut);
+
+  inputFile.pipe(createUnzip()).pipe(outputFile);
+  console.log("decompresses archive.gz ");
 };
 
 await decompress();
